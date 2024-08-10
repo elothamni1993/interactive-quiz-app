1bd800f82fab5cbe18763788f547a38bb2053590
the0zero0 <cryptologia6@gmail.com>
2024-08-10 19:41:27 +0100

    app

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from config import Config
 
 app = Flask(__name__)
-app.config.from_object('config')
+app.config.from_object(Config)  # Load configuration from Config class
 db = SQLAlchemy(app)
 
 from app import routes, models
-
