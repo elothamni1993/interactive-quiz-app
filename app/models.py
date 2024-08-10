5c9383a58f8d481eb26dd6b97ea7fc7c8b358573
the0zero0 <cryptologia6@gmail.com>
2024-08-10 20:01:22 +0100

    app

--- app/models.py
+++ app/models.py
@@ -1,11 +1,11 @@
 from app import db
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), unique=True, nullable=False)
-
-class Quiz(db.Model):
+class QuizQuestion(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     question = db.Column(db.String(200), nullable=False)
+    option1 = db.Column(db.String(100), nullable=False)
+    option2 = db.Column(db.String(100), nullable=False)
+    option3 = db.Column(db.String(100), nullable=False)
+    option4 = db.Column(db.String(100), nullable=False)
     answer = db.Column(db.String(100), nullable=False)
 
