b365ba6336903e1f37e89cd387308344efb5bc57
the0zero0 <cryptologia6@gmail.com>
2024-08-10 20:49:35 +0100

    app

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
-from app import app
+from app import app, db
+from flask_migrate import Migrate
 
-if __name__ == '__main__':
+migrate = Migrate(app, db)
+
+if __name__ == "__main__":
     app.run(debug=True)
-
