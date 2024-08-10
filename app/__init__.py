43ad7a325e66b6de2fe6593c04a3d6b965b0c08d
the0zero0 <cryptologia6@gmail.com>
2024-08-10 22:33:31 +0100

    app

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,21 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from config import Config
+from flask_migrate import Migrate
 
-app = Flask(__name__)
-app.config.from_object(Config)
+db = SQLAlchemy()
+migrate = Migrate()
 
-db = SQLAlchemy(app)
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object('config.Config')
+    
+    db.init_app(app)
+    migrate.init_app(app, db)
 
-from app import routes, models
+    with app.app_context():
+        from . import routes  # Import routes
+        from .models import QuizQuestion  # Import models
+        
+        db.create_all()
 
+    return app
